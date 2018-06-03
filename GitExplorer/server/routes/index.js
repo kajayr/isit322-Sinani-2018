803c7717e78e5620e191d15c51e060bbc75abb14
Your Name <you@example.com>
2018-06-02 23:11:37 -0700

    SystemdServices.sh

--- GitExplorer/server/routes/index.js
+++ GitExplorer/server/routes/index.js
@@ -7,4 +7,8 @@ router.get('/', function (req, res, next) {
     res.render('index', {title: 'server'});
 });
 
+router.get('/qux', function(request, response, next) {
+    requester('http://localhost:30027/').pipe(response);
+});
+
 module.exports = router;
